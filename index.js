4ca00ccd9c6f9f15897c5d6deda32a69fbb90fa0
Espen Kalvatn <espenk@stacc.com>
2023-03-17 14:38:57 +0100

    completed exercise 3.2

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ app.get('/', (req, res) => {
   res.send('<h1>Phonebook backend</h1>')
 })
 
+app.get('/info', (req, res) => {
+  res.send(`
+    <p>Phonebook  has info for ${persons.length} pepople</p>
+    <p>${new Date()}</p>
+  `)
+})
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
